a70a942e4bb91dcc481fac0834e9161a895abea1
zinc <zincsamael@gmail.com>
2016-02-14 13:44:23 -0600

    Add - testing add method

--- src/JavaBigIntegerTester.java
+++ src/JavaBigIntegerTester.java
@@ -3,9 +3,11 @@ import java.math.BigInteger;
 public class JavaBigIntegerTester {
 
 	public static void main(String[] args) {
-		BigInteger a = new BigInteger("999");
-		System.out.println(a.pow(8));
+		BigInteger a = new BigInteger("992027944069944027992001");
+		System.out.println(a.multiply(new BigInteger("2")));
 
+		LargeInteger l = new LargeInteger("992027944069944027992001");
+		System.out.println(LargeInteger.add(l, l));
 	}
 
 }
